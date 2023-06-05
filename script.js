6f207ec70948a4cf4b9e0ef478f4f68db3f0aa62
Kilox6991 <4inf.mpromero@gmail.com>
2023-06-05 11:02:18 +0200

    Resultados

--- script.js
+++ script.js
@@ -2,19 +2,19 @@
 
 // 1.1 Crea una variable developer1 y asignale un nombre cualquiera.
 
-// AQUÍ TU CÓDIGO
+//let developer1 = "Manuel"
 
 // 1.2.Imprime en consola el texto "La primera desarrolladora se llama ****"
 
-// AQUÍ TU CÓDIGO
+// console.log("La primera desarrolladora se llama Paula")
 
 // 1.3.Crea una variable developer2 y asigna otro nombre.
 
-// AQUÍ TU CÓDIGO
+//let developer2 = "Paula"
 
 // 1.4. Imprime en consola "El segundo desarrollador se llama ****"
 
-// AQUÍ TU CÓDIGO
+// console.log("El segundo desarrollador se llama Pepe")
 
 // ITERACIÓN 2: CONDICIONALES
 
@@ -26,23 +26,44 @@
 
 // "Qué casualidad, ambos nombres tienen ** caracteres".
 
-// AQUÍ TU CÓDIGO
+/*if (developer1.legth > developer2.legth) {
+    console.log("El nombre" + developer1 + "es el más largo con" + developer1.legth + "caracteres")
+}
+else if (developer2.leght > developer2.leght) {
+    console.log("El nombre" + developer2 + "es el más largo con" + developer2.legth + "caracteres")
+}
+else
+    console.log ("Qué casualidad, ambos nombres tienen" + developer1.legth + "caracteres")*/
+
+
 
 // ITERACIÓN 3: FUNCIONES Y BUCLES
 
 // Descomenta la siguiente linea de código.
-
-// let input = prompt("Escribe un nombre");
+let input = prompt("Escribe un nombre");
 
 // 3.1 Declara una función que acepte una palabra como parámetro y la devuelva con todos sus caracteres en mayúsculas y separados por espacios. Ejemplo: Para "Ana" devolverá "A N A".
-
-// AQUÍ TU CÓDIGO
+function mayusculas(palabra) {
+    console.log(input.toUpperCase());  
+}
+mayusculas;
 
 // 3.2 Declara una función que acepte un nombre como parámetro y devuelva el número de palabras que tiene el nombre. Ejemplo: Para "Antonio Alberto Jesús" devolverá 3.
 
-// AQUÍ TU CÓDIGO
+function contarPalabras(nombre) {
+    let palabras = nombre.split(" ");
+    return palabras.length;
+}
+
+let nombre = "Antonio Alberto Jesús"
+let cantidad = contarPalabras(nombre);
+console.log("Numero de palabras " + cantidad)
 
 // 3.3 Declara una tercera función llamada printUserDetails que haga uso de las dos funciones anteriores y reciba la entrada del usuario en el prompt. La función debe imprimir en consola la siguiente salida:
+function printUserDetails(params) {
+   console.log(mayusculas); 
+   console.log(contarPalabras);
+}
 
 // Para la entrada "Antonio Alberto Jesús" la consola imprimirá:
 
@@ -52,7 +73,7 @@ DETALLES para 'Antonio Alberto Jesus':
 Número de palabras 3:
 - A N T O N I O (7 caracteres)
 - A L B E R T O (7 caracteres)
-- J E S U S (5 caracteres) 
+- J E S U S (5 caracteres)
 
 */
 
